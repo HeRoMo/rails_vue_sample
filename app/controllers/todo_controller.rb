153935553d1d213d6c7179026bd282ace508774f
HeRoMo <hero@asterisk-works.jp>
2017-06-10 07:24:42 +0900

    add Todo controller

---
+++ app/controllers/todo_controller.rb
@@ -0,0 +1,16 @@
+class TodoController < ApplicationController
+  def list
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
---
+++ spec/views/todo/destroy.html.slim_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "todo/destroy.html.slim", type: :view do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
