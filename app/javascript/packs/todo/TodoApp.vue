c99c04b128d09d76dcb482ab1957d23c18621b61
HeRoMo <hero@asterisk-works.jp>
2017-06-11 17:02:35 +0900

    implements basic todo list

---
+++ app/javascript/packs/todo/TodoApp.vue
@@ -0,0 +1,59 @@
+<template lang="pug">
+  #todo-app
+    h2 TODO List
+    ul
+      todo-item(v-for="todo in todos", :todo="todo", :key="todo.id")
+
+</template>
+
+<script>
+  'use strict';
+  import axios from 'axios';
+  import TodoItem from './TodoItem.vue'
+  export default {
+
+    props: {
+
+    },
+    components:{
+      'todo-item': TodoItem
+    },
+    data: function(){
+      return { todos: []}
+    },
+    methods: {
+
+    },
+    // ライフサイクルフック
+    beforeCreate(){
+      console.log("beforeCreate")
+    },
+    created(arg) {
+      console.log("Created")
+      console.log(this)
+      axios.get('//localhost:3000/todo/list').then(
+        res => {
+          this.todos = res.data
+        }
+      )
+    },
+    beforeMount(){
+      console.log("beforeMount")
+    },
+    mounted(){
+      console.log("mounted")
+    },
+    beforeUpdate(){
+      console.log("beforeUpdate")
+
+    },
+    updated(){
+      console.log("updated")
+    }
+  }
+</script>
+
+<style lang="sass" scoped>
+  h2
+    border-bottom: solid 1px black
+</style>
---
+++ app/javascript/packs/todo/TodoItem.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  li
+    div(:class="{done: todo.done}")
+      input(type="checkbox", @change="change", v-model="todo.done")
+      | {{ todo.title }}
+
+</template>
+
+<script>
+  'use strict';
+  import axios from 'axios'
+
+  export default {
+    props:['todo'],
+    methods: {
+      change(){
+        axios.put('//localhost:3000/todo/'+ this.todo.id, this.todo)
+      }
+    }
+  }
+
+</script>
+
+<style lang="sass" scoped>
+  li
+    text-decoration: none
+    list-style: none inside
+    border: 1px solid #CCCCCC
+    margin-bottom: 3px
+  .done
+    text-decoration: line-through
+
+</style>
